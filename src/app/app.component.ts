dd23a7259fb37c135934c164e9e8917431f753d5
CSchulz <christian@schulz.re>
2017-05-11 13:13:49 +0200

    enable type-check and restrict-plus-operands and no-unused-variable rule added external dependency

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LogLevel, LogMessage, LogFormat, MessageFormatUtils } from 'typescript-logging';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title: string = 'app';
+
+  public ngOnInit(): void {
+    this.title += this.method(new Date()) + ' ' + AppComponent.method2(new Date());
+    let result: string = '';
+    result += this.method(new Date()) + ' ' + AppComponent.method2(new Date());
+  }
+
+  private method(arg1: any): string {
+    return `${arg1}`;
+  }
+
+  private static method2(arg1: any): string {
+    return `${arg1}`;
+  }
+
+  protected createDefaultLogMessage(msg: LogMessage): string {
+    const format: LogFormat = msg.logGroupRule.logFormat;
+    let result: string = '';
+    if (format.showTimeStamp) {
+      result += MessageFormatUtils.renderDate(msg.date, format.dateFormat) + ' ';
+    }
+
+    result += LogLevel[msg.level].toUpperCase() + ' ';
+    if (format.showLoggerName) {
+      result += `[${msg.loggerName}]`;
+    }
+
+    result += ' ' + msg.message;
+    if (msg.errorAsStack !== null) {
+      result += '\n' + msg.errorAsStack;
+    }
+
+    return result;
+  }
 }
